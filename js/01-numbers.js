7f239d02e6660a13bdf9da89f5922ab9f5de1b6a
bozenakr <bozena.krasevic@gmail.com>
2022-10-18 13:05:16 +0300

    Initial commit

---
+++ js/01-numbers.js
@@ -0,0 +1,16 @@
+console.log('Numbers...');
+const begalybe = Infinity;
+console.log(begalybe);
+
+
+const day1 = 5;
+const day2 = 15;
+const day3 = 100;
+const totalSum = day1 + day2 + day3;
+console.log(totalSum);
+
+
+const n1 = 1.1;
+const n2 = 0.1;
+const n3 = -7.13;
+const n4 = 1;
---
+++ js/02-string.js
@@ -0,0 +1,33 @@
+// String - tekstas
+
+
+const firstName = 'Petras';
+console.log(firstName);
+
+const lastName = 'Petraitis';
+console.log(lastName);
+
+const fullName = firstName + ' ' + lastName;
+console.log(fullName);
+
+
+const kabutes = 'Cia tures buti kabutes';
+console.log(kabutes);
+
+
+
+console.log(2+2);
+console.log('2' + '2');
+console.log('2' + 2);
+
+
+
+const multiline = 'Labas rytas,\Lietuva!';
+console.log(multiline);
+
+const multiline1 = 'Labas rytas,\n\Lietuva!';
+console.log(multiline1);
+
+const multiline2 = 'Labas rytas,\n\r\Lietuva!';
+console.log(multiline2);
+
