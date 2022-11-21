6e25c2155bcffa30d7a0b6970bb9c284a26a2501
bozenakr <bozena.krasevic@gmail.com>
2022-11-21 12:49:59 +0200

    array with numbers  from 1 to 10 in random order

--- js/01-numbers.js
+++ js/01-numbers.js
@@ -250,3 +250,30 @@ console.log(+{ a: 1 });
 console.log(+{ a: '1' });
 console.log(+{ 1: 'a' });
 console.log(+{ 1: 1 });
+
+
+console.clear();
+//PHP uzduotis-testas
+// masyvas 10 elementu atsitiktiniai nuo 1 iki 11 kiekviena karta sugeneruoja naujus atsitiktinius skaicius
+
+//===========1 sprendimas==========
+// let masyvas = [];
+
+// for (let i = 0; i < 12; i++) {
+//     masyvas.push(Math.floor(Math.random() * 11) + 1)
+//     // masyvas.push(i);
+// }
+// console.log(masyvas);
+
+//========2 sprendimas=============
+function random(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1) + min);
+}
+const masyvas = [...Array(10)].map(() => random(1, 11));
+
+console.log(masyvas);
+
+
+
